35865f1ba4f93427571e38e99a2a7e384f2edc11
Priyanshu Gupta <pg798308@gmail.com>
2024-09-30 18:09:19 +0530

    Changes

--- app/admin/addBlog/page.jsx
+++ app/admin/addBlog/page.jsx
@@ -1,11 +1,11 @@
-'use client'
-import { assets } from '@/Assets/assets'
-import axios from 'axios'
-import Image from 'next/image'
-import React, { useState } from 'react'
-import { toast } from 'react-toastify'
+'use client';
+import { assets } from '@/Assets/assets';
+import axios from 'axios';
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
-const page = () => {
+const Page = () => {
 
     const [image,setImage] = useState(false);
     const [data,setData] = useState({
@@ -74,4 +74,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page;
